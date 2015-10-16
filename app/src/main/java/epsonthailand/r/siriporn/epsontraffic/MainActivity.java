5469d3b40019ab7fd75efdd566311ab36893c192
Siriporn.R <siriporn.r@eth.epson.co.t>
2015-10-16 11:15:13 +0700

    Button Controller Finish

--- app/src/main/java/epsonthailand/r/siriporn/epsontraffic/MainActivity.java
+++ app/src/main/java/epsonthailand/r/siriporn/epsontraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package epsonthailand.r.siriporn.epsontraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,7 +24,30 @@ public class MainActivity extends AppCompatActivity {
        //Bind Widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
     } // Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.pig);
+                buttonMediaPlayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/VDcPRPo2Sik"));
+                        startActivity(objIntent);
+                
+
+            }   //event
+        });
+    }
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);

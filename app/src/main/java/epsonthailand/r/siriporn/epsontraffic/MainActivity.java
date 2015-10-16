d3bce9fc463b5e39773b6cd7db750a85b7c3fb1a
Siriporn.R <siriporn.r@eth.epson.co.t>
2015-10-16 17:01:12 +0700

    Process Success

--- app/src/main/java/epsonthailand/r/siriporn/epsontraffic/MainActivity.java
+++ app/src/main/java/epsonthailand/r/siriporn/epsontraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -21,13 +22,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       //Bind Widget
+        //Bind Widget
         bindWidget();
 
         //Button Controller
         buttonController();
 
+        //Create ListView
+        createListView();
+
     } // Main Method
+
+    private void createListView() {
+
+        //Setup Source of Image
+        final int[] iconInts = new int[20];
+        iconInts[0] = R.drawable.traffic_01;
+        iconInts[1] = R.drawable.traffic_02;
+        iconInts[2] = R.drawable.traffic_03;
+        iconInts[3] = R.drawable.traffic_04;
+        iconInts[4] = R.drawable.traffic_05;
+        iconInts[5] = R.drawable.traffic_06;
+        iconInts[6] = R.drawable.traffic_07;
+        iconInts[7] = R.drawable.traffic_08;
+        iconInts[8] = R.drawable.traffic_09;
+        iconInts[9] = R.drawable.traffic_10;
+        iconInts[10] = R.drawable.traffic_11;
+        iconInts[11] = R.drawable.traffic_12;
+        iconInts[12] = R.drawable.traffic_13;
+        iconInts[13] = R.drawable.traffic_14;
+        iconInts[14] = R.drawable.traffic_15;
+        iconInts[15] = R.drawable.traffic_16;
+        iconInts[16] = R.drawable.traffic_17;
+        iconInts[17] = R.drawable.traffic_18;
+        iconInts[18] = R.drawable.traffic_19;
+        iconInts[19] = R.drawable.traffic_20;
+
+        final String[] title1Strings = getResources().getStringArray(R.array.title1);
+
+        String[] title2Strings = getResources().getStringArray(R.array.title2);
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, iconInts, title1Strings, title2Strings);
+
+        trafficListView.setAdapter(objMyAdapter);
+
+        //Active When Click
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class);
+                objIntent.putExtra("Title", title1Strings[i]);
+                objIntent.putExtra("Image", iconInts[i]);
+                objIntent.putExtra("Index", i);
+                startActivity(objIntent);
+
+            }
+        });
+
+
+
+    }
 
     private void buttonController() {
 
@@ -42,8 +97,8 @@ public class MainActivity extends AppCompatActivity {
                 //Intent to WebView
                 Intent objIntent = new Intent(Intent.ACTION_VIEW);
                 objIntent.setData(Uri.parse("https://youtu.be/VDcPRPo2Sik"));
-                        startActivity(objIntent);
-                
+                startActivity(objIntent);
+
 
             }   //event
         });
@@ -51,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
-        aboutMeButton = (Button)findViewById(R.id.button);
+        aboutMeButton = (Button) findViewById(R.id.button);
     }
 
 }   // Main Class
